0ef6df368219f7431cbe95911546a78984f4de0a
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-16 15:28:33 +0200

    Update 2

--- src/com/jjozwik/BankAccount.java
+++ src/com/jjozwik/BankAccount.java
@@ -1,5 +1,8 @@
 package com.jjozwik;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static com.jjozwik.ThreadColor.ANSI_RED;
 import static com.jjozwik.ThreadColor.ANSI_RESET;
 import static com.jjozwik.ThreadColor.ANSI_WHITE;
@@ -8,23 +11,79 @@ public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
+//    public synchronized void deposit(double amount){
+//        balance += amount;
+//        System.out.println(amount + " has been deposited to the bank account: " + this.accountNumber );
+//    }
+//
+//    public synchronized void withdraw(double amount){
+//        balance -= amount;
+//        System.out.println(amount + " has been taken from the bank account: " + this.accountNumber );
+//    }
+//
+//    public synchronized void printAccountBalance(){
+//        System.out.println(ANSI_RESET + "Current account state = " + this.balance );
+//    }
+
+
+//    public void deposit(double amount){
+//        synchronized (this){
+//            balance += amount;
+//            System.out.println(amount + " has been deposited to the bank account: " + this.accountNumber );
+//        }
+//
+//    }
+//
+//    public void withdraw(double amount){
+//        synchronized (this){
+//            balance -= amount;
+//            System.out.println(amount + " has been taken from the bank account: " + this.accountNumber );
+//        }
+//
+//    }
+//
+//    public void printAccountBalance(){
+//        System.out.println(ANSI_RESET + "Current account state = " + this.balance );
+//    }
+
     public void deposit(double amount){
-        balance += amount;
-        System.out.println(amount + " has been deposited to the bank account: " + this.accountNumber );
+        lock.lock();
+        try {
+            balance += amount;
+            System.out.println(amount + " has been deposited to the bank account: " + this.accountNumber );
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void withdraw(double amount){
-        balance -= amount;
-        System.out.println(amount + " has been taken from the bank account: " + this.accountNumber );
+        lock.lock();
+        try{
+            balance -= amount;
+            System.out.println(amount + " has been taken from the bank account: " + this.accountNumber );
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void printAccountBalance(){
         System.out.println(ANSI_RESET + "Current account state = " + this.balance );
+    }
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber(){
+        System.out.println("Account number = "+accountNumber);
     }
 }
--- src/com/jjozwik/Main.java
+++ src/com/jjozwik/Main.java
@@ -1,5 +1,6 @@
 package com.jjozwik;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static com.jjozwik.ThreadColor.ANSI_BLUE;
@@ -7,7 +8,6 @@ import static com.jjozwik.ThreadColor.ANSI_RED;
 
 public class Main {
 
-    public static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
 
@@ -17,35 +17,28 @@ public class Main {
         Thread customer1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (lock){
                     System.out.println(ANSI_RED + "Customer 1 ");
                     account.deposit(300.00);
                     System.out.println(ANSI_RED + "Customer 1 ");
                     account.withdraw(50.00);
                     account.printAccountBalance();
-                }
-;            }
+            }
         });
 
         Thread customer2 = new Thread(new Runnable() {
             @Override
 
             public void run() {
-                synchronized (lock){
                     System.out.println(ANSI_BLUE + "Customer 2 ");
                     account.deposit(203.75);
-//                account.printAccountBalance();
                     System.out.println(ANSI_BLUE + "Customer 2 ");
                     account.withdraw(100.00);
                     account.printAccountBalance();
-                }
             }
         });
 
-
         customer1.start();
         customer2.start();
-//        account.printAccountBalance();
 
     }
 
